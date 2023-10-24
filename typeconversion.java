21551b74262df1fd9b1efeb1715285b1bf1bf031
singh04priyansh <148889371+singh04priyansh@users.noreply.github.com>
2023-10-24 21:06:40 +0530

    Add files via upload

---
+++ typeconversion.java
@@ -0,0 +1,14 @@
+
+
+public class typeconversion {
+    public static void main (String args[]){
+      // Scanner sc = new Scanner(System.in);
+       // int a = sc.nextInt();
+       // int b = sc.nextInt();
+        //int sum = a*b;
+        float side = 8.14f;
+        int i = (int) side;
+        System.out.println(i);
+    }
+    
+}
